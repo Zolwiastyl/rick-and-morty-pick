d311328014e732391ad70bbdb5bd15fbe964b476
Zolwiastyl <jakub.zolw.kwiatek@gmail.com>
2020-03-25 22:06:17 +0100

    That's what Piotrek helped

--- src/TasksLists.tsx
+++ src/TasksLists.tsx
@@ -2,13 +2,121 @@ import React from "react";
 import { StateProps } from "./interfaces";
 import { Task, TasksStateProps } from "./types";
 import styled, { createGlobalStyle, css } from "styled-components";
+import { motion } from "framer-motion";
+
+// api
 
 const HOST: string = "https://zolwiastyl-todoapp.builtwithdark.com";
 
 const tasksRequest = new Request(HOST + "/tasks");
 const newTaskPostURL = new Request(HOST + "/tasks");
 
-export function TasksLists(props: TasksStateProps) {
+function sendNewTask(task: Task) {
+  fetch(newTaskPostURL, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      name: task.name,
+      status: task.status
+    })
+  });
+}
+
+// end api
+
+interface TaskButtonProps {
+  onClick: () => void;
+  children: React.ReactNode;
+}
+function TaskButton({ children, onClick }: TaskButtonProps) {
+  return (
+    <button className={`move-to-button`} onClick={onClick}>
+      {children}
+    </button>
+  );
+}
+
+interface GroupOfButtonsProps {
+  task: Task;
+  statuses: Status[];
+  moveToAnotherGroup: (status: string, task: Task) => void;
+}
+function GroupOfButtons({ task, statuses }: GroupOfButtonsProps) {
+  return (
+    <motion.div
+      className="task-item"
+      drag={true}
+      onDragEnd={(event, info) => {}}
+    >
+      <p className="task-name">{task.name}</p>
+      <div className="buttons-group">
+        move to:
+        {statuses
+          .filter(currentStatus => currentStatus.statusName != task.status)
+          .map(status => (
+            <TaskButton onClick={() => moveToAnotherGroup(status, task)}>
+              {status.statusName}
+            </TaskButton>
+          ))}
+      </div>
+    </motion.div>
+  );
+}
+
+interface TaskListProps {
+  statusName: string;
+  tasks: Task[];
+  statuses: Status[];
+  moveToAnotherGroup: (status: string, task: Task) => void;
+}
+function TaskList({ statusName, tasks, statuses }: TaskListProps) {
+  console.log(statusName.split("").join(""));
+  console.log("TaskList ran");
+  console.log(statusName);
+  //props.tasks.map(element =>console.log({ key: element.name, value: element.status }));
+  return (
+    <div className={statusName}>
+      <p className="group-heading"> // {statusName} //</p>
+      {tasks
+        .filter(task => task.status == statusName)
+        .map(task => (
+          <GroupOfButtons
+            task={task}
+            statuses={statuses}
+            moveToAnotherGroup={moveToAnotherGroup}
+          />
+        ))}
+    </div>  }
+  }
+  }
+  }
+
+  return <TaskItem />;
+  return <TaskItem />;
+}
+
+  );
+}
+
+function TaskItem() {
+  // list of status może być tutaj podawany w propie
+  const TaskItem = listOfStatus.map(status => (
+    <div className="tasks-group">
+      <TaskList statusName={status.statusName} tasks={tasks} />
+    </div>
+  ));
+
+  return <div className="TaskButtontasks-list-item">{TaskItem}</div>;
+}
+
+type Status = {
+  statusName: string;
+};
+
+export function TasksLists({ tasks, setTasks }: TasksStateProps) {
   const listOfStatus: Array<Status> = [
     { statusName: "todo" },
     { statusName: "working-on-it" },
@@ -16,95 +124,31 @@ export function TasksLists(props: TasksStateProps) {
     { statusName: "stuck" },
     { statusName: "done" }
   ];
-  type Status = {
-    statusName: string;
-  };
 
-  function TaskItem() {
-    const TaskItem = listOfStatus.map(status => (
-      <div className="tasks-group">
-        <TaskList statusName={status.statusName} />
-      </div>
-    ));
+  function moveToAnotherGroup(status: string, task: Task) {
+    // WHAT IF SERVER UPDATE FAILS?
 
-    function TaskList(taskProps: Status) {
-      console.log(taskProps.statusName.split("").join(""));
-      console.log("TaskList ran");
-      console.log(taskProps.statusName);
-      //props.tasks.map(element =>console.log({ key: element.name, value: element.status }));
-      return (
-        <div>
-          <p className="group-heading"> // {taskProps.statusName} //</p>
-          {props.tasks
-            .filter(task => task.status == taskProps.statusName)
-            .map(task => (
-              <GroupOfButtons name={task.name} status={task.status} />
-            ))}
-        </div>
-      );
-    }
-    function GroupOfButtons(task: Task) {
-      return (
-        <div className="task-item">
-          <p className="task-name">{task.name}</p>
-          <div className="buttons-group">
-            move to:
-            {listOfStatus
-              .filter(currentStatus => currentStatus.statusName != task.status)
-              .map(status => (
-                <ButtonElement name={task.name} status={status.statusName} />
-              ))}
-          </div>
-        </div>
-      );
-    }
-    function ButtonElement(props: Task) {
-      return (
-        <button
-          id={props.name}
-          className={`move-to-button`}
-          onClick={event => moveTo(props.status, event.currentTarget.id)}
-        >
-          {props.status}
-        </button>
-      );
-    }
-    return <div className="tasks-list-item">{TaskItem}</div>;
-  }
-  function moveTo(status: string, buttonId: string) {
-    const tasksArrayToSave = props.tasks.slice();
-    const task = buttonId;
+    // 1.
+    // UPDATE ON SERVER
+    // THEN IF IT SUCCEEDS UPDATE LOCAL COPY
+    // OTHERWISE DISPLAY ERROR
+    // 2. (OPTIMISTIC UPDATE)
+    // UPDATE ON SERVER,
+    // UPDATE LOCAL COPY WHILE YOU WAIT,
+    // IF SERVER UPDATE FAILS,
+    // ROLLBACK YOUR LOCAL UPDATE
+    // AND DISPLAY ERROR
 
-    const found = tasksArrayToSave.find(
-      (element: Task) => element.name == task
+    // UPDATE ON SERVER
+    sendNewTask({ name: task.name, status: status }); // TODO: To się może nie powieść.
+
+    // UPDATE LOCAL COPY
+    setTasks(
+      tasks.map(t =>
+        t.name !== task.name ? t : { name: task.name, status: status }
+      )
     );
-
-    tasksArrayToSave.splice(tasksArrayToSave.indexOf(found as Task), 1);
-    sendNewTask({ name: task, status: status });
-    tasksArrayToSave.unshift({ name: task, status: status });
-    props.setTasks(tasksArrayToSave);
   }
-  function sendNewTask(task: Task) {
-    fetch(newTaskPostURL, {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        name: task.name,
-        status: task.status
-      })
-    });
-  }
-  return <TaskItem />;
-}
 
-function makeStatusNameValidCSSClassName(statusName: string) {
-  return statusName.split("").join("");
+  return null;
 }
-
-type TaskListProps = {
-  status: string;
-  taskState: TasksStateProps;
-};
