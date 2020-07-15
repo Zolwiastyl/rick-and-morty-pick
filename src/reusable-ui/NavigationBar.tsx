ed7348acef917db132303b34a3bb1f0fdf763de8
Zolwiastyl <jakub.zolw.kwiatek@gmail.com>
2020-07-15 12:14:51 +0200

    Add div props to navbar

--- src/reusable-ui/NavigationBar.tsx
+++ src/reusable-ui/NavigationBar.tsx
@@ -1,16 +1,16 @@
-import React, { FunctionComponent } from "react";
+import React, { ComponentProps, FunctionComponent } from "react";
 
 type NavBarProps = {
 	children: React.ReactNode;
-};
+} & ComponentProps<"div">;
 
 export const NavigationBar: FunctionComponent<NavBarProps> = ({ children }) => {
 	return (
 		<div
 			className="md:flex md:flex-col flex flex-row md:relative 
 			bg-gray-900 md:h-full md:min-h-screen  md:p-5 md:w-24
-			md:space-x-0 md:space-y-2
-			space-x-2
+			md:space-x-0 space-y-3
+			space-x-2 p-2
 			md:rounded-r-lg md:justify-start  items-center justify-end
 			sm:relative"
 		>
