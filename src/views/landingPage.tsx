454ae6ee2c8c85ff7890741adfa7c4839e854b7e
Zolwiastyl <jakub.zolw.kwiatek@gmail.com>
2020-07-16 12:35:01 +0200

    move text in landing page to the middle

--- src/views/landingPage.tsx
+++ src/views/landingPage.tsx
@@ -36,12 +36,30 @@ export const LandingPage: React.FC = () => {
 	};
 
 	return (
-		<div className="flex flex-col">
+		<div className="flex middle  flex-col">
 			<div className="bg-gray-900 justify-end space-y-2 s	pace-x-2 p-2 flex ">
 				<LogInButton />
 			</div>
 
-			<div> landing page and else</div>
+			<div className=" flex flex-col self-center justify-center  w-full p-10 text-l text-gray-800 text-justify tracking-wide space-x-3 space-y-4 leading-loose max-w-4xl">
+				{" "}
+				<h1 className="self-center  text-2xl"> Phloem App </h1>
+				<div className=" justify-center max-w-4xl">
+					This is my first todo app, that I wanted to do because it
+					provides a user with possibility to visualize tasks and their
+					dependencies with graphs.
+				</div>
+				<h2 className=" self-center text-xl"> Main idea </h2>
+				<div className=" ">
+					When I was trying to use any of already avaliable solutions I
+					kept bumping into place where I couldn't plan my tasks. Of
+					course, I could divide them in groups or tables but I still
+					couldn't think of them in organised manner. I realized that when
+					using just a pen and paper drawing tasks as graph where they are
+					clearly leading one to another is natural.
+				</div>
+				<h2></h2>
+			</div>
 		</div>
 	);
 };
