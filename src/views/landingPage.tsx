7a790b02c3166e7bcd9a68c4ece635181c5fd2bf
Zolwiastyl <jakub.zolw.kwiatek@gmail.com>
2020-07-22 16:20:58 +0200

    Add optional header and icon rendering

--- src/views/landingPage.tsx
+++ src/views/landingPage.tsx
@@ -1,72 +1,67 @@
 import { RedirectLoginOptions } from "@auth0/auth0-spa-js";
 import React, { ComponentProps, Fragment, ReactNode, useEffect } from "react";
-import { LogIn, LogOut } from "react-feather";
+import { GitHub, LogIn, LogOut, Twitter } from "react-feather";
 
 import appLogo from "../assets/app-icon.svg";
-import whyIcon from "../assets/idea-icon.svg";
+import exclamation from "../assets/exclamation.svg";
+import questionMark from "../assets/idea-icon.svg";
+import lightbulb from "../assets/lightbulb.svg";
+import lightining from "../assets/lightining.svg";
+import tree from "../assets/tree.svg";
 import { useAuth0 } from "../react-auth0-spa";
 import { IconButton } from "../reusable-ui/IconButton";
 
-type TextChunkProps = {
-	headerText: string;
+type SectionWithIconProps = {
+	icon?: string;
+	headerText?: string;
 	paragraphText?: string;
 	text?: React.ReactNode;
-	colorName: string;
 } & ComponentProps<"section">;
-const TextArticle: React.FC<TextChunkProps> = ({
-	headerText,
-	paragraphText,
-	colorName,
-	text,
-}) => {
-	return (
-		<section className={colorName}>
-			<div className="grid grid-cols-1 space-y-5">
-				<h1 className=" text-2xl text-center "> {headerText} </h1>
-				<div className=" justify-center text-xl max-w-4xl py-10">
-					{paragraphText}
-					{text}
-				</div>
-			</div>
-		</section>
-	);
-};
-type SectionWithIconProps = {
-	icon: string;
-} & TextChunkProps;
+
+let lastColorWasDark = false;
+
 const SectionWithIcon: React.FC<SectionWithIconProps> = ({
 	headerText,
 	paragraphText,
-	colorName,
-	text,
+	children,
 	icon,
 }) => {
-	const colorOfHeaderBackground =
-		colorName === "dark" ? "bg-gray-100" : "bg-white";
+	const colorOfHeaderBackground = lastColorWasDark
+		? "bg-gray-100"
+		: "bg-white";
+	const mainColorOfChunk = lastColorWasDark ? "dark" : "light";
+	lastColorWasDark = !lastColorWasDark;
 	return (
-		<section className={colorName}>
-			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+		<section className={mainColorOfChunk}>
+			<div className="grid grid-cols-1 w-full md:grid-cols-2 lg:grid-cols-3">
 				<div
 					className={
 						"flex flex-row justify-center md:relative sticky top-0 " +
 						colorOfHeaderBackground
 					}
 				>
-					<img
-						src={icon}
-						alt="app-logo"
-						className="md:h-64 h-20 object-center flex-shrink-0 self-center sticky top-0"
-					></img>
-					<h1 className=" text-2xl text-center md:hidden block py-5 px-3 sticky top-0">
-						{headerText}{" "}
-					</h1>
+					{icon ? (
+						<img
+							src={icon}
+							alt={headerText + " graphics"}
+							className="md:h-64 h-20 object-center flex-shrink-0 self-center sticky top-0"
+						></img>
+					) : null}
+					{headerText ? (
+						<h1 className=" text-2xl text-center md:hidden block py-5 px-3 sticky top-0">
+							{headerText}{" "}
+						</h1>
+					) : null}
 				</div>
 				<div>
-					<h1 className=" text-2xl text-center hidden md:block ">
-						{headerText}{" "}
-					</h1>
-					<div className=" justify-center text-xl max-w-4xl py-10">
+					{headerText ? (
+						<h1 className=" text-2xl text-center hidden md:block ">
+							{headerText}{" "}
+						</h1>
+					) : null}
+					<div className=" justify-center text-xl max-w-4xl min-w-4xl py-10">
 						{paragraphText}
+						{children}
 					</div>
 				</div>
 				<div className="hidden md:block" />
@@ -109,78 +104,73 @@ export const LandingPage: React.FC = () => {
 				<LogInButton />
 			</div>
 
-			<main className="grid grid-cols-1 w-full  text-l text-gray-800 text-justify tracking-wide space-y-4 leading-loose">
+			<main className="grid grid-cols-1 w-full  text-l text-gray-800 text-justify tracking-wide leading-loose">
 				{" "}
+				<SectionWithIcon icon={appLogo} headerText={"Phloem App"}>
+					This is my first todo app. I wanted to do it because it provides
+					a user with possibility to visualize tasks and their dependencies
+					with graphs.
+				</SectionWithIcon>
 				<SectionWithIcon
-					icon={appLogo}
-					paragraphText={
-						"This is my first todo app. I wanted to do it because it provides a user with possibility to visualize tasks and their dependencies with graphs."
-					}
-					headerText={"Phloem App"}
-					colorName="dark"
-				/>
-				{/* <section className="dark">
-					<div className="grid grid-cols-3">
-						<div className="flex flex-row justify-center">
-							<img
-								src={appLogo}
-								alt="app-logo"
-								className="h-64 object-center flex-shrink-0 "
-							></img>
-						</div>
-
-						<div>
-							<h1 className=" text-3xl text-center ">Phloem App </h1>
-							<div className=" justify-center text-xl max-w-4xl py-10">
-								
-							</div>
-						</div>
-						<div />
-					</div>
-				</section> */}
-				<SectionWithIcon
-					icon={whyIcon}
-					paragraphText={secondParagraphText}
-					headerText={"Why this app?"}
-					colorName="light"
-				/>
-				<TextArticle
-					paragraphText={secondParagraphText}
-					headerText={"Main idea"}
-					colorName={"dark"}
-				/>
-				<TextArticle
-					colorName={"light"}
-					headerText={"Header"}
-					paragraphText={
-						"Omnia Galia est divisa in partes tres quarum unam incolum Belgae, aliam Aqutianiae tertiam qui incolunt lingua celtae nostra Galli apellantur "
-					}
+					icon={questionMark}
+					headerText={"Why next todo app?"}
 				>
-					<div>text from children</div>
+					When I was using any of already avaliable solutions I kept
+					bumping into place where I could not <b>plan</b> my tasks. I
+					didn't know why - I have divided them in groups and tables, gave
+					them colors and icons - however I didn't find them handy.
+				</SectionWithIcon>
+				<SectionWithIcon icon={exclamation} headerText="Found it">
+					One day I've realized that instead of wirtting my tasks to app I
+					was drawing them as graph with a pencil.
+				</SectionWithIcon>
+				<SectionWithIcon icon={lightbulb} headerText={"Solution"}>
+					Why not make app out of it that will make it easier!
+				</SectionWithIcon>
+				<SectionWithIcon icon={tree}>
+					Omnia Galia est divisa in partes tres quarum unam incolum Belgae,
+					aliam Aqutianiae tertiam qui incolunt lingua celtae nostra Galli
+					apellantur.
 					<button
 						onClick={() => {
 							client?.loginWithRedirect(redirectOption);
 						}}
+						className=" font-bold hover:scale-x-90 hover:transform hover:translate-x-4"
 					>
 						Try it
 					</button>
-				</TextArticle>
-				<TextArticle
-					colorName={"dark"}
-					headerText={"Header"}
-					paragraphText={"whole paraghraph"}
-				/>
-				<TextArticle
-					colorName={"light"}
-					headerText={"trixy"}
-					paragraphText={"another paragraph text"}
-				>
-					<span>Another paragraph with text inside</span>
-				</TextArticle>
+				</SectionWithIcon>
+				<SectionWithIcon icon={lightining}>
+					Omnia Galia est divisa in partes tres quarum unam incolum Belgae,
+					aliam Aqutianiae tertiam qui incolunt lingua celtae nostra Galli
+					apellantur.
+				</SectionWithIcon>
 			</main>
+			<footer className="flex justify-center text-gray-200  bg-gray-800 tracking-widest p-5 px-10">
+				<div className="grid grid-cols-3 w-full md:w-1/2 lg:w-1/3 p-10">
+					<span className="lg:p-1"> Find me on: </span>
+					<div className="">
+						<a
+							href="https://twitter.com/zolwiastyl"
+							target="_blank"
+							rel="noopener noreferrer"
+							className="flex justify-center"
+						>
+							<Twitter className="stroke-current hover:text-primary-600" />
+						</a>
+					</div>
+					<div>
+						<a
+							className="flex justify-center"
+							href="https://github.com/zolwiastyl"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							<GitHub className="hover:text-gray-900" />
+						</a>
+					</div>
+				</div>
+			</footer>
 		</div>
 	);
 };
-
-const secondParagraphText =
-	"When I was trying to use any of already avaliable solutions I kept bumping into place where I could not plan my tasks. Of course, I could divide them in groups or tables but I still couldn't think of them in organised manner. I realized that when using just a pen and paper drawing tasks as graph where they are clearly leading one to another is natural.";
